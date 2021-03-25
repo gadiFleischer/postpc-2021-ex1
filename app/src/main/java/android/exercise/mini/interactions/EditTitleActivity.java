ea90605ca0e3a28945ff808abec79f527aa9dfcf
gadi <gadi.fleischer@mail.huji.ac.il>
2021-03-25 10:25:36 +0200

    finished animation

--- app/src/main/java/android/exercise/mini/interactions/EditTitleActivity.java
+++ app/src/main/java/android/exercise/mini/interactions/EditTitleActivity.java
@@ -1,12 +1,9 @@
 package android.exercise.mini.interactions;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EditTitleActivity extends AppCompatActivity {
@@ -17,13 +14,11 @@ public class EditTitleActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_edit_title);
-
     // find all views
     FloatingActionButton fabStartEdit = findViewById(R.id.fab_start_edit);
     FloatingActionButton fabEditDone = findViewById(R.id.fab_edit_done);
     TextView textViewTitle = findViewById(R.id.textViewPageTitle);
     EditText editTextTitle = findViewById(R.id.editTextPageTitle);
-
     // setup - start from static title with "edit" button
     fabStartEdit.setVisibility(View.VISIBLE);
     fabEditDone.setVisibility(View.GONE);
@@ -31,27 +26,17 @@ public class EditTitleActivity extends AppCompatActivity {
     textViewTitle.setVisibility(View.VISIBLE);
     editTextTitle.setText("Page title here");
     editTextTitle.setVisibility(View.GONE);
-
     // handle clicks on "start edit"
     fabStartEdit.setOnClickListener(v -> {
       isEditing=true;
+      fabStartEdit.animate().alpha(0F).setDuration(500L).start();
       fabStartEdit.setVisibility(View.GONE);
       textViewTitle.setVisibility(View.GONE);
       fabEditDone.setVisibility(View.VISIBLE);
+      //animation
+      fabEditDone.setAlpha(0F);
+      fabEditDone.animate().alpha(1F).setDuration(500L).start();
       editTextTitle.setVisibility(View.VISIBLE);
-
-      /*
-      TODO:
-      1. animate out the "start edit" FAB
-      2. animate in the "done edit" FAB
-      3. hide the static title (text-view)
-      4. show the editable title (edit-text)
-      5. make sure the editable title's text is the same as the static one
-      6. optional (HARD!) make the keyboard to open with the edit-text focused,
-          so the user can start typing without the need another click on the edit-text
-
-      to complete (1.) & (2.), start by just changing visibility. only add animations after everything else is ready
-       */
     });
 
     // handle clicks on "done edit"
@@ -61,21 +46,14 @@ public class EditTitleActivity extends AppCompatActivity {
       textViewTitle.setText(newTitle);
       editTextTitle.setText(newTitle);
       //return to main activity
+      fabEditDone.animate().alpha(0F).setDuration(500L).start();
       fabStartEdit.setVisibility(View.VISIBLE);
       textViewTitle.setVisibility(View.VISIBLE);
       fabEditDone.setVisibility(View.GONE);
       editTextTitle.setVisibility(View.GONE);
-      /*
-      TODO:
-      1. animate out the "done edit" FAB
-      2. animate in the "start edit" FAB
-      3. take the text from the user's input in the edit-text and put it inside the static text-view
-      4. show the static title (text-view)
-      5. hide the editable title (edit-text)
-      6. make sure that the keyboard is closed
-
-      to complete (1.) & (2.), start by just changing visibility. only add animations after everything else is ready
-       */
+      //animation
+      fabStartEdit.setAlpha(0F);
+      fabStartEdit.animate().alpha(1F).setDuration(500L).start();
     });
   }
 
@@ -86,7 +64,6 @@ public class EditTitleActivity extends AppCompatActivity {
     FloatingActionButton fabEditDone = findViewById(R.id.fab_edit_done);
     TextView textViewTitle = findViewById(R.id.textViewPageTitle);
     EditText editTextTitle = findViewById(R.id.editTextPageTitle);
-
     if(isEditing){
       fabStartEdit.setVisibility(View.VISIBLE);
       textViewTitle.setVisibility(View.VISIBLE);
